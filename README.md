dab267aa66fb81497e71e32f0f0371a3ad340017
jyoung105 <jeongmin1604@gmail.com>
2025-04-22 19:12:52 +0900

    Remove all project files and configurations, including ESLint, Git ignore, license, Next.js configuration, package files, and application source code.

--- README.md
+++
@@ -1,3 +0,0 @@
-# Open Flamel
-
-Build an interactive Designer, Flamel.
--- app/src/tokenizer.js
+++
@@ -1,5 +0,0 @@
-import llamaTokenizer from "llama-tokenizer-js";
-
-export const countTokens = (text) => {
-    return llamaTokenizer.encode(text).length;
-};
